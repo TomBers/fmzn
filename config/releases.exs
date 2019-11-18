5af512cb5166bc156a9d7bfdc1dbc7599a704120
Tom Berman <tjcberman@gmail.com>
2019-11-18 20:05:42 +0000

    Add DB name

--- config/releases.exs
+++ config/releases.exs
@@ -3,6 +3,7 @@ import Config
 db_user = System.get_env("DB_USER", "postgres")
 db_password = System.get_env("DB_PASSWORD", "postgres")
 db_host = System.get_env("DB_HOST", "localhost")
+db_name = System.get_env("DB_NAME", "fmzn_prod")
 
 IO.inspect(db_host)
 IO.inspect(db_user)
@@ -15,6 +16,6 @@ config :fmzn, FmznWeb.Endpoint,
 config :fmzn, Fmzn.Repo,
   username: db_user,
   password: db_password,
-  database: "fmzn_prod",
+  database: db_name,
   hostname: db_host,
   pool_size: 15
