e39667230339c348967a3effc03c90ebfd9d06d9
Tom Berman <tjcberman@gmail.com>
2020-01-16 11:05:34 +0000

    remove ability to edit products and advert from web

--- lib/fmzn_web/controllers/product_controller.ex
+++ lib/fmzn_web/controllers/product_controller.ex
@@ -11,22 +11,6 @@ defmodule FmznWeb.ProductController do
     render(conn, "index.html", products: products)
   end
 
-  def new(conn, _params) do
-    changeset = Resources.change_product(%Product{productpoints: [%ProductPoints{}, %ProductPoints{}, %ProductPoints{}]})
-    render(conn, "new.html", changeset: changeset)
-  end
-
-  def create(conn, %{"product" => product_params}) do
-    case Resources.create_product(product_params) do
-      {:ok, product} ->
-        conn
-        |> put_flash(:info, "Product created successfully.")
-        |> redirect(to: Routes.product_path(conn, :show, product))
-
-      {:error, %Ecto.Changeset{} = changeset} ->
-        render(conn, "new.html", changeset: changeset)
-    end
-  end
 
   def show(conn, %{"id" => id}) do
     product = Resources.get_product_by_slug!(id)
@@ -53,32 +37,5 @@ defmodule FmznWeb.ProductController do
 
   end
 
-  def edit(conn, %{"id" => id}) do
-    product = Resources.get_product!(id)
-    changeset = Resources.change_product(product)
-    render(conn, "edit.html", product: product, changeset: changeset)
-  end
 
-  def update(conn, %{"id" => id, "product" => product_params}) do
-    product = Resources.get_product!(id)
-
-    case Resources.update_product(product, product_params) do
-      {:ok, product} ->
-        conn
-        |> put_flash(:info, "Product updated successfully.")
-        |> redirect(to: Routes.product_path(conn, :show, product.slug))
-
-      {:error, %Ecto.Changeset{} = changeset} ->
-        render(conn, "edit.html", product: product, changeset: changeset)
-    end
-  end
-
-  def delete(conn, %{"id" => id}) do
-    product = Resources.get_product!(id)
-    {:ok, _product} = Resources.delete_product(product)
-
-    conn
-    |> put_flash(:info, "Product deleted successfully.")
-    |> redirect(to: Routes.product_path(conn, :index))
-  end
 end
