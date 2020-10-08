964bba944f310421bb2ae2562e344c071b0e35a1
Tom Berman <tjcberman@gmail.com>
2020-10-08 16:12:59 +0100

    Get products the same height

--- lib/fmzn_web/views/page_view.ex
+++ lib/fmzn_web/views/page_view.ex
@@ -5,6 +5,10 @@ defmodule FmznWeb.PageView do
     Float.floor(val, 2)
   end
 
+  def currency_format_pounds(val) do
+    Kernel.trunc(val)
+  end
+
   def max_description_length(description) do
     String.slice(description, 0..150)
   end
