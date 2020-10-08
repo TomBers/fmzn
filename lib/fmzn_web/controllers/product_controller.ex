abf4bcf3f1c49496f60bb78b21beae6fdecf65f7
Tom Berman <tjcberman@gmail.com>
2020-10-08 11:38:50 +0100

    Add product form

--- lib/fmzn_web/controllers/product_controller.ex
+++ lib/fmzn_web/controllers/product_controller.ex
@@ -11,6 +11,27 @@ defmodule FmznWeb.ProductController do
     render(conn, "index.html", products: products)
   end
 
+  def new(conn, _params) do
+    changeset = Resources.change_product(%Product{productpoints: [%ProductPoints{}, %ProductPoints{}, %ProductPoints{}]})
+    render(conn, "new.html", changeset: changeset)
+  end
+
+  def create(conn, %{"product" => product_params}) do
+    product_params = Map.put(product_params, "slug", product_params["title"] |> String.downcase |> String.replace(" ", "-"))
+    product_params = Map.put(product_params, "discount", Enum.random([0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]))
+
+    case Resources.create_product(product_params) do
+      {:ok, _product} ->
+        conn
+        |> put_flash(:info, "Product created successfully.")
+        |> redirect(to: Routes.page_path(conn, :index))
+
+      {:error, %Ecto.Changeset{} = changeset} ->
+        IO.inspect(changeset)
+        render(conn, "new.html", changeset: changeset)
+    end
+  end
+
 
   def show(conn, %{"id" => id}) do
     product = Resources.get_product_by_slug!(id)
