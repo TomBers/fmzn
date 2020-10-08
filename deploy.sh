7408b2c898dcb501688f0bf2b7c67662b8e3d470
Tom Berman <tjcberman@gmail.com>
2020-10-08 16:21:40 +0100

    Fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
+mv assets/node_modules assets/tmp_node_modules
+sleep 5
 heroku container:push web
 sleep 5
 heroku container:release web
 
+mv assets/tmp_node_modules assets/node_modules
